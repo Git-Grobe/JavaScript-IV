3678a2dbeaffc3b2447655480ef6239c5022b97b
Scott Grobe <gitgrobe@gmail.com>
2019-01-27 13:40:44 -0500

    add custom objects for lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -23,8 +23,11 @@ class Instructor extends Person{
     demo(subject) {
        return `Today we are learning ${subject}`;
     }
-    grade(subject) {
+    grade(student, subject) {
         return `${student.name} receives a perfect score on ${subject}`
+    }
+    randomGrade(student) {
+        return `${student.grade}`
     }
 }
 
@@ -34,28 +37,111 @@ class Student extends Person{
         this.previousBackground = attrs.previousBackground;
         this.className = attrs.className;
         this.favSubjects = attrs.favSubjects;
+        this.grade = 100;
     }
-    listsSubjects(){
-        return `${this.favSubjects}`
+    listSubjects(subject) {
+        return this.favSubjects;
     }
     PRAssignment(subject) {
-        return `${student.name} has submitted a PR for ${subject}`
+        return `${this.name} has submitted a PR for ${subject}`
     }
     sprintChallenge(subject) {
-        return `${student.name} has begun sprint challenge for ${subject}`
+        return `${this.name} has begun sprint challenge for ${subject}`
     }
 }
 
-class ProjectManagers extends Instructor{
+class ProjectManager extends Instructor{
     constructor(attrs) {
         super(attrs);
         this.gradClassName = attrs.gradClassName;
         this.favInstructor = attrs.favInstructor;
     }
-    standup(channel) {
-        return `${name} announces to ${channel}, @channel standy times!`
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!`
     }
-    debugsCode(subject) {
-        return `${name} debugs ${student.name}'s code on ${subject}!`
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}!`
     }
 }
+
+const instructorOne = new Instructor({
+    name: '2pac',
+    age: 25,
+    location: 'Los Angeles',
+    gender: 'Male',
+    specialty: 'Rapping',
+    favLanguage: 'West coast slang',
+    catchPhrase: 'West-siiide',
+
+})
+
+const instructorTwo = new Instructor({
+    name: 'Biggie',
+    age: 25,
+    location: 'New York',
+    gender: 'Male',
+    specialty: 'Slanging Dope',
+    favLanguage: 'East coast slang',
+    catchPhrase: 'Kick in the door, waving the four four',
+})
+
+const studentOne = new Student({
+    name: 'Future Hndrxx',
+    age: 27,
+    location: 'Atlanta',
+    gender: 'Male',
+    previousBackground: 'Pill popper',
+    className: 'Purple Reign',
+    favSubjects: ['Stacking Chips', 'Double cupping']
+  })
+
+const studentTwo = new Student({
+    name: 'French Montana',
+    age: 30,
+    location: 'South Bronx',
+    gender: 'Male',
+    previousBackground: 'Coke Boyz',
+    className: 'Excuse My French',
+    favSubjects: ['Off the Rip', 'Stay Scheming']
+})
+
+const pmOne = new ProjectManager({
+    name: 'Diddy',
+    age: 50,
+    location: 'New York City',
+    gender: 'Male',
+    specialty: 'Making Hits',
+    favLanguage: 'Gangsta Talk',
+    catchPhrase: 'Cant nobody hold me down',
+    gradClassName: 'BadBoy',
+    favInstructor: 'Christopher Wallace'
+})
+
+  
+const pmTwo = new ProjectManager({
+    name: 'Noreaga',
+    age: 45,
+    location: 'Iraq',
+    gender: 'Male',
+    specialty: 'Getting bloody money',
+    favLanguage: 'Spanish',
+    catchPhrase: 'What What'
+})
+  
+
+
+
+
+
+
+
+
+console.log(instructorOne.speak());
+console.log(instructorOne.demo('Stuff'));
+console.log(instructorOne.grade(studentOne, 'Purple Reign'));
+console.log(studentOne.speak());
+console.log(studentTwo.listSubjects());
+console.log(studentTwo.PRAssignment('Slanging Dope'));
+console.log(studentOne.sprintChallenge('Going Platinum'));
+console.log(pmTwo.standUp('channel'));
+console.log(pmOne.grade(studentTwo, 'Flying'));
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -58,7 +58,7 @@ Prototype Refactor
         this.dimensions = game.dimensions;
       }
       destroy() {
-        return 'Object was removed from the game.';
+        return `${this.name} was removed from the game.`; 
       }
   };
   
@@ -147,7 +147,7 @@ Prototype Refactor
   // Test you work by un-commenting these 3 objects and the list of console logs below:
   
   
-    const mage = new Humanoid({
+const mage = new Humanoid({
       createdAt: new Date(),
       dimensions: {
         length: 2,
@@ -161,9 +161,9 @@ Prototype Refactor
         'Staff of Shamalama',
       ],
       language: 'Common Tongue',
-    });
+});
   
-    const swordsman = new Humanoid({
+const swordsman = new Humanoid({
       createdAt: new Date(),
       dimensions: {
         length: 2,
@@ -178,9 +178,9 @@ Prototype Refactor
         'Shield',
       ],
       language: 'Common Tongue',
-    });
+});
   
-    const archer = new Humanoid({
+const archer = new Humanoid({
       createdAt: new Date(),
       dimensions: {
         length: 1,
@@ -195,21 +195,21 @@ Prototype Refactor
         'Dagger',
       ],
       language: 'Elvish',
-    });
+});
   
-    console.log(mage.createdAt); // Today's date
-    console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-    console.log(swordsman.healthPoints); // 15
-    console.log(mage.name); // Bruce
-    console.log(swordsman.team); // The Round Table
-    console.log(mage.weapons); // Staff of Shamalama
-    console.log(archer.language); // Elvish
-    console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-    console.log(mage.takeDamage()); // Bruce took damage.
-    console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
   
   
-    // Stretch task: 
-    // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
-    // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-    // * Create two new objects, one a villain and one a hero and fight it out with methods!
+// Stretch task: 
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
